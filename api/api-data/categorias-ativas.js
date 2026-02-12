0e7cca58bc0d80ee1bee82fa9da87f8c354537d3
peagasfx <pedrogcubes@gmail.com>
2026-02-11 23:08:08 -0300

    Update categorias-ativas.js

--- api/api-data/categorias-ativas.js
+++ api/api-data/categorias-ativas.js
@@ -1,308 +1,55 @@
-[
-  {
-    "id": 1,
-    "nome": "Geral",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 2,
-    "nome": "Air Fryers",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 3,
-    "nome": "Alto-Falantes",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 4,
-    "nome": "Resfriamento",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 5,
-    "nome": "Tratamento do Ar",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 6,
-    "nome": "Esportes e Jogos ao Ar Livre",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 7,
-    "nome": "Fornos e Churrasqueiras ElÃ©tricas",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 8,
-    "nome": "Grelhas e AcessÃ³rios",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 9,
-    "nome": "Bancos, Cadeiras e Banquetas",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 10,
-    "nome": "Outros",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 11,
-    "nome": "Aspiradores de PÃ³ e UtensÃ­lios de Cuidados com o Piso",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 12,
-    "nome": "Tablets",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 13,
-    "nome": "Ciclismo",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 14,
-    "nome": "Futebol, Futsal e Sepak Takraw",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 15,
-    "nome": "Acampamento e Caminhada",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 16,
-    "nome": "Armazenamento de Alimentos",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 17,
-    "nome": "Playstation",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 18,
-    "nome": "Carrinho e Sistemas de Viagem",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 19,
-    "nome": "Bolsas e Bagagens",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 20,
-    "nome": "Chuveiros, Chuveirinhos e BidÃªs",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 21,
-    "nome": "Lavadoras de LouÃ§a",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 22,
-    "nome": "Perfumes Femininos",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 23,
-    "nome": "UtensÃ­lios Capilares",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 24,
-    "nome": "Micro-ondas",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 25,
-    "nome": "Fritadeiras",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 26,
-    "nome": "Equipamento Fitness",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 41,
-    "nome": "Esportes de Prancha",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 42,
-    "nome": "Geladeiras",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 43,
-    "nome": "Monitores",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 44,
-    "nome": "MÃ¡quinas de Lavar e Secadoras",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 45,
-    "nome": "Bagagens",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 46,
-    "nome": "Ferros de Passar e Vaporizadores",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 47,
-    "nome": "Smartwatches e Fitness Trackers",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 48,
-    "nome": "Tv Boxes & Receptores",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 50,
-    "nome": "Modens e Roteadores de Internet",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 51,
-    "nome": "Ferramentas",
-    "ativo": 1,
-    "ordem": 0
-  },
-  {
-    "id": 27,
-    "nome": "Material Escolar",
-    "ativo": 1,
-    "ordem": 1
-  },
-  {
-    "id": 28,
-    "nome": "EletrodomÃ©sticos ",
-    "ativo": 1,
-    "ordem": 2
-  },
-  {
-    "id": 29,
-    "nome": "Panelas",
-    "ativo": 1,
-    "ordem": 2
-  },
-  {
-    "id": 49,
-    "nome": "Capacetes",
-    "ativo": 1,
-    "ordem": 3
-  },
-  {
-    "id": 30,
-    "nome": "Mochilas",
-    "ativo": 1,
-    "ordem": 5
-  },
-  {
-    "id": 31,
-    "nome": "UtensÃ­lios de Cuidados Faciais",
-    "ativo": 1,
-    "ordem": 6
-  },
-  {
-    "id": 32,
-    "nome": "Jogos de Dados,Tabuleiro e Cartas",
-    "ativo": 1,
-    "ordem": 8
-  },
-  {
-    "id": 33,
-    "nome": "AcessÃ³rios de Maquiagem",
-    "ativo": 1,
-    "ordem": 9
-  },
-  {
-    "id": 34,
-    "nome": "LenÃ§Ã³is e Fronhas de Travesseiros",
-    "ativo": 1,
-    "ordem": 11
-  },
-  {
-    "id": 35,
-    "nome": "Garrafas de Ãgua e AcessÃ³rios",
-    "ativo": 1,
-    "ordem": 13
-  },
-  {
-    "id": 36,
-    "nome": "EletrÃ´nicos",
-    "ativo": 1,
-    "ordem": 15
-  },
-  {
-    "id": 37,
-    "nome": "Moda",
-    "ativo": 1,
-    "ordem": 16
-  },
-  {
-    "id": 38,
-    "nome": "Casa e Jardim",
-    "ativo": 1,
-    "ordem": 17
-  },
-  {
-    "id": 39,
-    "nome": "Esportes",
-    "ativo": 1,
-    "ordem": 18
-  },
-  {
-    "id": 40,
-    "nome": "Beleza",
-    "ativo": 1,
-    "ordem": 19
-  }
-]
+module.exports = (req, res) => {
+  res.status(200).json([
+    { id: 1, nome: "Geral", ativo: 1, ordem: 0 },
+    { id: 2, nome: "Air Fryers", ativo: 1, ordem: 0 },
+    { id: 3, nome: "Alto-Falantes", ativo: 1, ordem: 0 },
+    { id: 4, nome: "Resfriamento", ativo: 1, ordem: 0 },
+    { id: 5, nome: "Tratamento do Ar", ativo: 1, ordem: 0 },
+    { id: 6, nome: "Esportes e Jogos ao Ar Livre", ativo: 1, ordem: 0 },
+    { id: 7, nome: "Fornos e Churrasqueiras Elétricas", ativo: 1, ordem: 0 },
+    { id: 8, nome: "Grelhas e Acessórios", ativo: 1, ordem: 0 },
+    { id: 9, nome: "Bancos, Cadeiras e Banquetas", ativo: 1, ordem: 0 },
+    { id: 10, nome: "Outros", ativo: 1, ordem: 0 },
+    { id: 11, nome: "Aspiradores de Pó e Utensílios de Cuidados com o Piso", ativo: 1, ordem: 0 },
+    { id: 12, nome: "Tablets", ativo: 1, ordem: 0 },
+    { id: 13, nome: "Ciclismo", ativo: 1, ordem: 0 },
+    { id: 14, nome: "Futebol, Futsal e Sepak Takraw", ativo: 1, ordem: 0 },
+    { id: 15, nome: "Acampamento e Caminhada", ativo: 1, ordem: 0 },
+    { id: 16, nome: "Armazenamento de Alimentos", ativo: 1, ordem: 0 },
+    { id: 17, nome: "Playstation", ativo: 1, ordem: 0 },
+    { id: 18, nome: "Carrinho e Sistemas de Viagem", ativo: 1, ordem: 0 },
+    { id: 19, nome: "Bolsas e Bagagens", ativo: 1, ordem: 0 },
+    { id: 20, nome: "Chuveiros, Chuveirinhos e Bidês", ativo: 1, ordem: 0 },
+    { id: 21, nome: "Lavadoras de Louça", ativo: 1, ordem: 0 },
+    { id: 22, nome: "Perfumes Femininos", ativo: 1, ordem: 0 },
+    { id: 23, nome: "Utensílios Capilares", ativo: 1, ordem: 0 },
+    { id: 24, nome: "Micro-ondas", ativo: 1, ordem: 0 },
+    { id: 25, nome: "Fritadeiras", ativo: 1, ordem: 0 },
+    { id: 26, nome: "Equipamento Fitness", ativo: 1, ordem: 0 },
+    { id: 41, nome: "Esportes de Prancha", ativo: 1, ordem: 0 },
+    { id: 42, nome: "Geladeiras", ativo: 1, ordem: 0 },
+    { id: 43, nome: "Monitores", ativo: 1, ordem: 0 },
+    { id: 44, nome: "Máquinas de Lavar e Secadoras", ativo: 1, ordem: 0 },
+    { id: 45, nome: "Bagagens", ativo: 1, ordem: 0 },
+    { id: 46, nome: "Ferros de Passar e Vaporizadores", ativo: 1, ordem: 0 },
+    { id: 47, nome: "Smartwatches e Fitness Trackers", ativo: 1, ordem: 0 },
+    { id: 48, nome: "TV Boxes & Receptores", ativo: 1, ordem: 0 },
+    { id: 50, nome: "Modems e Roteadores de Internet", ativo: 1, ordem: 0 },
+    { id: 51, nome: "Ferramentas", ativo: 1, ordem: 0 },
+    { id: 27, nome: "Material Escolar", ativo: 1, ordem: 1 },
+    { id: 28, nome: "Eletrodomésticos", ativo: 1, ordem: 2 },
+    { id: 29, nome: "Panelas", ativo: 1, ordem: 2 },
+    { id: 49, nome: "Capacetes", ativo: 1, ordem: 3 },
+    { id: 30, nome: "Mochilas", ativo: 1, ordem: 5 },
+    { id: 31, nome: "Utensílios de Cuidados Faciais", ativo: 1, ordem: 6 },
+    { id: 32, nome: "Jogos de Dados, Tabuleiro e Cartas", ativo: 1, ordem: 8 },
+    { id: 33, nome: "Acessórios de Maquiagem", ativo: 1, ordem: 9 },
+    { id: 34, nome: "Lençóis e Fronhas de Travesseiros", ativo: 1, ordem: 11 },
+    { id: 35, nome: "Garrafas de Água e Acessórios", ativo: 1, ordem: 13 },
+    { id: 36, nome: "Eletrônicos", ativo: 1, ordem: 15 },
+    { id: 37, nome: "Moda", ativo: 1, ordem: 16 },
+    { id: 38, nome: "Casa e Jardim", ativo: 1, ordem: 17 },
+    { id: 39, nome: "Esportes", ativo: 1, ordem: 18 },
+    { id: 40, nome: "Beleza", ativo: 1, ordem: 19 }
+  ]);
+};
