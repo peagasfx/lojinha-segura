1a27b99b75ef1d187ddd7e3d16ab2afc2bf87427
peagasfx <pedrogcubes@gmail.com>
2026-02-11 22:40:30 -0300

    Delete api/api-data directory

--- api/api-data/configuracoes.js
+++
@@ -1,5 +0,0 @@
-import config from '../../../data/configuracoes.json';
-
-export default function handler(req, res) {
-  res.status(200).json(config);
-}
--- api/api-data/produtos.js
+++
@@ -1,5 +0,0 @@
-import produtos from '../../../data/produtos.json';
-
-export default function handler(req, res) {
-  res.status(200).json(produtos);
-}
